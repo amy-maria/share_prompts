403a75d9d14efaf1e735752fedd696823eb31242
amy-maria <122695916+amy-maria@users.noreply.github.com>
2023-06-18 19:24:19 -0400

    Corrected page errors

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,6 @@
 import "@styles/globals.css";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
   title: "Promptomatic",
@@ -6,12 +8,15 @@ export const metadata = {
 };
 
 const RootLayout = ({ children }) => (
-  <html lang="en">
+  <html lang='en'>
     <body>
-      <div className="main">
-        <div className="gradient" />
+      <div className='main'>
+        <div className='gradient' />
       </div>
-      <main className="app">{children}</main>
+      <main className='app'>
+        <Nav />
+        {children}
+      </main>
     </body>
   </html>
 );
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+"use client"
+import Link from "next/link";
+import image from "next/image";
+import { useState, useEffect } from "react";
+import { signIn, signOut, useSession, getProviders} from next-auth/react;s
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return <nav className="flex-between w-full mb-16 pt-3">
+    <Link href="/" className="flex gap-2 flex-center">
+    <Image src="/assets/images/logo.svg"
+    alt="Promptopia Logo"
+    width={30}
+    height={30}
+    className="object-contain"
+     />
+
+
+    </Link>
+   </nav>;
 };
 
 export default Nav;
